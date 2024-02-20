0ae08ffa30bf5d7043499c44d087abbb7fbfdcfd
Hassen-Chtara <hassen.chtara@supcom.tn>
2024-02-20 17:02:35 +0100

    Add operations

--- src/zz/mutator.py
+++ src/zz/mutator.py
@@ -2,47 +2,77 @@ from .operations import *
 import os
 
 
-class Mutator :
-    def __init__(self,corpus=set(),dictionary=set()):
+class Mutator:
+    def __init__(self, corpus=set(), dictionary=set()):
         self.corpus = corpus
         self.dictionary = dictionary
 
     def is_corpus_empty(self):
         return not self.corpus
 
-    def add_to_corpus(self,x):
+    def is_dictionary_empty(self):
+        return not self.dictionary
+
+    def add_to_corpus(self, x):
         self.corpus.add(x)
 
-    def add_to_dictionary(self,x):
+    def add_to_dictionary(self, x):
         self.dictionary.add(x)
 
     def get_random_seed(self):
         return random.choice(list(self.corpus))
 
-    def load_to_corpus(self,path):
-        if os.path.isfile(path) :
+    def load_to_corpus(self, path):
+        if os.path.isfile(path):
             self.load_file_to_corpus(path)
         self.load_directory_to_corpus(path)
 
-    def load_file_to_corpus(self,path):
-        with open(path,'rb') as f:
+    def load_file_to_corpus(self, path):
+        with open(path, "rb") as f:
             self.add_to_corpus(f.read())
 
-    def load_directory_to_corpus(self,path):
-        files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-        for file in files :
+    def load_directory_to_corpus(self, path):
+        files = [
+            os.path.join(path, f)
+            for f in os.listdir(path)
+            if os.path.isfile(os.path.join(path, f))
+        ]
+        for file in files:
             self.load_file_to_corpus(file)
 
-    def mutate(self,seed=None,times=5):
-        if seed :
+    def select_seed(self, seed):
+        if seed:
             x_ = seed
-        elif not self.is_corpus_empty() :
+        elif not self.is_corpus_empty():
             x_ = self.get_random_seed()
-        else :
+        else:
             ## raise : no seed to mutate
-            raise('No seed to mutate!! mutate() expectes an input or a value in the courpus')
+            raise (
+                "No seed to mutate!! mutate() expectes an input or a value in the courpus"
+            )
+        return x_
 
-        for _ in range(times) :
-            operation = random.choice([bytes_reversing, byte_deletion, byte_insertion, byte_shuffling])
+    def select_operation(self):
+        return random.choice(
+            [
+                bytes_reversing,
+                byte_deletion,
+                byte_insertion,
+                shuffling,
+                byte_duplication,
+                byte_flip,
+                bit_flip,
+                byte_change,
+                inject_bytes,
+            ]
+        )
+
+    def mutate(self, seed=None, times=0):
+        x_ = self.select_seed(seed)
+        if not times:
+            times = random.randint(2, 5)
+
+        for _ in range(times):
+            operation = self.select_operation()
             x_ = operation(x_)
         return x_
--- src/zz/operations.py
+++ src/zz/operations.py
@@ -1,27 +1,61 @@
-import random 
+import random
 import sys
 
-def bit_flip(x):
-    pass
 
-def byte_flip(x):
-    pass
+def get_random_slice(x: bytes) -> bytes:
+    index_ = random.randint(0, len(x) - 1)
+    slice_len = random.randint(1, len(x) - index_)
+    return x[index_ : index_ + slice_len]
 
-def bytes_reversing(x):
+
+def bit_flip(x: bytes) -> bytes:
+    index_ = random.randint(0, len(x) - 1)
+    mask = 1 << random.randint(0, 7)
+    return x[:index_] + bytes([x[index_] ^ mask]) + x[index_ + 1 : len(x)]
+
+
+def byte_flip(x: bytes) -> bytes:
+    index_ = random.randint(0, len(x) - 1)
+    return x[:index_] + bytes([x[index_] ^ 0xFF]) + x[index_ + 1 : len(x)]
+
+
+def bytes_reversing(x: bytes) -> bytes:
     return x[::-1]
 
-def byte_duplication(x):
-    pass
 
-def byte_deletion(x):
-    index_ = random.randint(0,len(x))
-    return x[:index_] + x[index_+1:len(x)]
+def byte_duplication(x: bytes) -> bytes:
+    random_byte = random.choice(x)
+    index_ = random.randint(0, len(x) - 1)
+    return (
+        x[:index_]
+        + bytes([random_byte]) * random.randint(0, len(x) // 2)
+        + x[index_ + 1 : len(x)]
+    )
 
-def byte_insertion(x):
-    index_ = random.randint(0,len(x))
-    return x[:index_ ] + random.randbytes(1) + x[index_ +1:len(x)]
 
-def byte_shuffling(x):
+def byte_deletion(x: bytes) -> bytes:
+    index_ = random.randint(0, len(x) - 1)
+    return x[:index_] + x[index_ + 1 : len(x)]
+
+
+def byte_change(x: bytes) -> bytes:
+    index_ = random.randint(0, len(x) - 1)
+    return x[:index_] + random.randbytes(1) + x[index_ + 1 : len(x)]
+
+
+def byte_insertion(x: bytes) -> bytes:
+    index_ = random.randint(0, len(x) - 1)
+    return x[:index_] + random.randbytes(1) + x[index_ : len(x)]
+
+
+def shuffling(x: bytes) -> bytes:
     x_ = list(x)
     random.shuffle(x_)
-    return b''.join([ i.to_bytes(1, sys.byteorder) for i in  x_])
+    return b"".join([i.to_bytes(1, sys.byteorder) for i in x_])
+
+
+def inject_bytes(x: bytes) -> bytes:
+    index_ = random.randint(0, len(x) - 1)
+    return (
+        x[:index_] + random.randbytes(random.randbytes(1)[0]) + x[index_ + 1 : len(x)]
+    )
